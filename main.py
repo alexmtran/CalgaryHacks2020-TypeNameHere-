5d6b8f0ebcad75d15b31235fe3be44d1cb296a48
Rupert Amodia <rupertraphael@live.com>
2020-02-16 11:11:44 -0700

    Improve Ux.

--- main.py
+++ main.py
@@ -5,29 +5,42 @@ from anytree import RenderTree
 
 courses = {}
 
-with open('courses.csv', newline='') as file:
-	coursesInFile = csv.DictReader(file, delimiter=',')
-	
-	for course in coursesInFile:
-		courses[course['course_code']] = course
+def displayChoices(choices):
+	for index, choice in enumerate(choices):
+		print(f'{index+1}. {choice.strip()}')
+
 
 def getPrerequisitesDeep(selectedCourse, courses, tree):
 	selectedCourse = courses[selectedCourse]
 
 	if('prerequisites' not in selectedCourse.keys() or selectedCourse['prerequisites']  == ''):
-		print(selectedCourse['course_code'])
 		return selectedCourse['course_code']
 
 	choices = selectedCourse['prerequisites'].split('|')
 
 	# If there's only one choice, automatically select that one.
-	if len(choices) > 1:
-		print(choices)
-		choice = input('Select a choice (starting at 0 from left to right):')
-	else:
-		choice = 0
+	name = ''
+	try:
+		name = selectedCourse['course_name']
+	except:
+		name = selectedCourse['course_code']
+	print('Prerequisites for: ' + name)
 
-	choice = choices[int(choice)]
+	displayChoices(choices)
+
+	if len(choices) > 1:
+		choiceIndex = int(input('Select a course that you\'re gonna/you have finished:'))
+		choiceIndex -= 1
+
+		numberOfOptions = len(choices)
+
+		while choiceIndex > numberOfOptions - 1 or choiceIndex < 0:
+			choiceIndex = int(input('Invalid option. Select a course that you\'re gonna/you have finished:'))
+			choiceIndex -= 1
+	else:
+		choiceIndex = 0
+
+	choice = choices[choiceIndex].strip()
 	prerequisites = choice.split(';')
 
 	for index, prerequisite in enumerate(prerequisites):
@@ -39,11 +52,6 @@ def getPrerequisitesDeep(selectedCourse, courses, tree):
 		})
 
 		if(prerequisite in courses):
-			print(prerequisite)
-
-			print('the tree at:' + str(index))
-			print(tree[index])
-
 			getPrerequisitesDeep(prerequisite, courses, tree[index]['children'])
 
 def getPrerequisites(selectedCourse, courses):
@@ -57,13 +65,32 @@ def getPrerequisites(selectedCourse, courses):
 
 	return prerequisites
 
-selectedCourse = input('select a course:')
+with open('courses.csv', newline='') as file:
+	coursesInFile = csv.DictReader(file, delimiter=',')
+	
+	for course in coursesInFile:
+		courses[course['course_code']] = course
+
+
+selectedCourse = ''
+
+print('--------------Welcome to the UCalgary Prerequisites Wizard!-----------')
+selectedCourse = input('Select a UCalgary course you want to generate a prerequisites-tree from:')
 
 prerequisitesTree = getPrerequisites(selectedCourse, courses)
-print(prerequisitesTree)
 
 importer = DictImporter()
 root = importer.import_(prerequisitesTree)
-print(RenderTree(root))
 
 RenderTreeGraph(root).to_picture("tree.png")
+print('`tree.png` has been generated.')
+
+while selectedCourse in courses:
+	selectedCourse = input('Select a UCalgary course you want to generate a prerequisites-tree from:')
+	prerequisitesTree = getPrerequisites(selectedCourse, courses)
+
+	importer = DictImporter()
+	root = importer.import_(prerequisitesTree)
+
+	RenderTreeGraph(root).to_picture("tree.png")
+	print('`tree.png` has been generated.')
